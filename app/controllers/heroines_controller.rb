d71ceefb130130295195a9e1b005ca6f0b53cea6
tunkhine126 <tunkhine126@gmail.com>
2019-05-24 10:48:28 -0400

    CRUSHED IT

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,30 @@
 class HeroinesController < ApplicationController
-  def index
-    @heroines = Heroine.all
-  end
+  
+    def index
+      @heroines = Heroine.all
+    end
+
+    def new
+      @heroine = Heroine.new
+    end 
+
+    def show 
+      @heroines = Heroine.find(params[:id])
+    end 
+
+    def create
+      @heroine = Heroine.new(heroine_params)
+      if @heroine.save
+        redirect_to heroine_path(@heroine)
+      else 
+        render :new
+      end 
+    end
+  end 
+
+  private 
+  
+  def heroine_params 
+    @heroine = params.require(:heroine).permit(:name, :super_power, :power_id)
+
 end
