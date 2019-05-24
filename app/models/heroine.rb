663c599703649198186285b38a0047dd60008fc8
github username <dekadekadeka@gmail.com>
2019-05-24 10:47:58 -0400

    I GOT RAILED

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,4 @@
 class Heroine < ApplicationRecord
+    validates :name, uniqueness: true
+    belongs_to :power
 end
---
+++ db/migrate/20190524140821_change_column.rb
@@ -0,0 +1,5 @@
+class ChangeColumn < ActiveRecord::Migration[5.1]
+  def change
+    rename_column :heroines, :power, :power_id
+  end
+end
