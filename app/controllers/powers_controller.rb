49d92cf5f43081f02ca6f3ad9889abf42cefb1ca
seerosecode <keshalakemusic@gmail.com>
2019-05-24 10:49:34 -0400

    OH EM GEE I DID IT

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,30 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  def new
+    @power = Power.new
+  end
+
+  def create
+    @power = Power.new
+    @power.name = params["power"][:name]
+    @power.description = params["power"][:description]
+    if @power.save
+      redirect_to power_path(@power)
+    else
+      render :new
+    end
+  end
+
+  def search
+    @power = Power.find(params[:id])
+    redirect_to power_path(@power)
+  end
+
+
 end
